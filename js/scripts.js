e976cdaf4fa3b254f624556a9771cd1056d01ab1
Brian Scherner <brianscherner@gmail.com>
2023-05-02 15:46:54 -0700

    Add window.onload function. Declare variable for form object. Declare form.onsubmit function with preventDefault function. Declare variable for voteMenu and under18Message, with each being set to the id of its corresponding div. Declare variable for age, making it match the input id 'age'. Declare else if statement displaying vote div class if age is over 18. If age equals 18, display window.alert with congratulatory message. If age is under 18, display under 18 div class.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+	const form = document.querySelector("form");
+	form.onsubmit = function(event) {
+		event.preventDefault();
+
+		let voteMenu = document.getElementById("age");
+		voteMenu.setAttribute("class", "hidden");
+		let under18Message = document.getElementById("under-18");
+		under18Message.setAttribute("class", "hidden");
+
+		const age = parseInt(document.querySelector("input#age").value);
+
+		if (age > 18) {
+			voteMenu.removeAttribute("class");
+		} else if (age === 18) {
+			window.alert("Congratulations! Participate in American democracy while it still lasts!");
+			voteMenu.removeAttribute("class");
+		} else {
+			under18Message.removeAttribute("class");
+		}
+	};
+};
